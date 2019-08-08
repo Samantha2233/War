2f187b3fbac12df4d9e63ba389e8f6ed390f739f
Samantha <sami.shackelford@gmail.com>
2019-08-08 15:52:20 -0700

    initial code: including deal, compare, and transfer cards

---
+++ js/main.js
@@ -0,0 +1,551 @@
+const cardsArr = [
+    {
+      name: 'King of Hearts',
+      suit: 'heart',
+      value: 13,
+      img: 'images/hearts/hearts-K.svg',
+      id: null
+    },
+    {
+      name: 'Queen of Hearts',
+      suit: 'heart',
+      value: 12,
+      img: 'images/hearts/hearts-Q.svg',
+      id: null
+    },
+    {
+      name: 'Jack of Hearts',
+      suit: 'heart',
+      value: 11,
+      img: 'images/hearts/hearts-J.svg',
+      id: null
+    },
+    {
+      name: 'Ten of Hearts',
+      suit: 'heart',
+      value: 10,
+      img: 'images/hearts/hearts-r10.svg',
+      id: null
+    },
+    {
+      name: 'Nine of Hearts',
+      suit: 'heart',
+      value: 9,
+      img: 'images/hearts/hearts-r09.svg',
+      id: null
+    },
+    {
+      name: 'Eight of Hearts',
+      suit: 'heart',
+      value: 8,
+      img: 'images/hearts/hearts-r08.svg',
+      id: null
+    },
+    {
+      name: 'Seven of Hearts',
+      suit: 'heart',
+      value: 7,
+      img: 'images/hearts/hearts-r07.svg',
+      id: null
+    },
+    {
+      name: 'Six of Hearts',
+      suit: 'heart',
+      value: 6,
+      img: 'images/hearts/hearts-r06.svg',
+      id: null
+    },
+    {
+      name: 'Five of Hearts',
+      suit: 'heart',
+      value: 5,
+      img: 'images/hearts/hearts-r05.svg',
+      id: null
+    },
+    {
+      name: 'Four of Hearts',
+      suit: 'heart',
+      value: 4,
+      img: 'images/hearts/hearts-r04.svg',
+      id: null
+    },
+    {
+      name: 'Three of Hearts',
+      suit: 'heart',
+      value: 3,
+      img: 'images/hearts/hearts-r03.svg',
+      id: null
+    },
+    {
+      name: 'Two of Hearts',
+      suit: 'heart',
+      value: 2,
+      img: 'images/hearts/hearts-r02.svg',
+      id: null
+    },
+    {
+      name: 'Ace of Hearts',
+      suit: 'heart',
+      value: 14,
+      img: 'images/hearts/hearts-A.svg',
+      id: null
+    },
+    {
+      name: 'King of Clubs',
+      suit: 'club',
+      value: 13,
+      img: 'images/clubs/clubs-K.svg',
+      id: null
+    },
+    {
+      name: 'Queen of Clubs',
+      suit: 'club',
+      value: 12,
+      img: 'images/clubs/clubs-Q.svg',
+      id: null
+    },
+    {
+      name: 'Jack of Clubs',
+      suit: 'club',
+      value: 11,
+      img: 'images/clubs/clubs-J.svg',
+      id: null
+    },
+    {
+      name: 'Ten of Clubs',
+      suit: 'club',
+      value: 10,
+      img: 'images/clubs/clubs-r10.svg',
+      id: null
+    },
+    {
+      name: 'Nine of Clubs',
+      suit: 'club',
+      value: 9,
+      img: 'images/clubs/clubs-r09.svg',
+      id: null
+    },
+    {
+      name: 'Eight of Clubs',
+      suit: 'club',
+      value: 8,
+      img: 'images/clubs/clubs-r08.svg',
+      id: null
+    },
+    {
+      name: 'Seven of Clubs',
+      suit: 'club',
+      value: 7,
+      img: 'images/clubs/clubs-r07.svg',
+      id: null
+    },
+    {
+      name: 'Six of Clubs',
+      suit: 'club',
+      value: 6,
+      img: 'images/clubs/clubs-r06.svg',
+      id: null
+    },
+    {
+      name: 'Five of Clubs',
+      suit: 'club',
+      value: 5,
+      img: 'images/clubs/clubs-r05.svg',
+      id: null
+    },
+    {
+      name: 'Four of Clubs',
+      suit: 'club',
+      value: 4,
+      img: 'images/clubs/clubs-r04.svg',
+      id: null
+    },
+    {
+      name: 'Three of Clubs',
+      suit: 'club',
+      value: 3,
+      img: 'images/clubs/clubs-r03.svg',
+      id: null
+    },
+    {
+      name: 'Two of Clubs',
+      suit: 'club',
+      value: 2,
+      img: 'images/clubs/clubs-r02.svg',
+      id: null
+    },
+    {
+      name: 'Ace of Clubs',
+      suit: 'club',
+      value: 14,
+      img: 'images/clubs/clubs-A.svg',
+      id: null
+    },
+    {
+      name: 'King of Diamonds',
+      suit: 'diamond',
+      value: 13,
+      img: 'images/diamonds/diamonds-K.svg',
+      id: null
+    },
+    {
+      name: 'Queen of Diamonds',
+      suit: 'diamond',
+      value: 12,
+      img: 'images/diamonds/diamonds-Q.svg',
+      id: null
+    },
+    {
+      name: 'Jack of Diamonds',
+      suit: 'diamond',
+      value: 11,
+      img: 'images/diamonds/diamonds-J.svg',
+      id: null
+    },
+    {
+      name: 'Ten of Diamonds',
+      suit: 'diamond',
+      value: 10,
+      img: 'images/diamonds/diamonds-r10.svg',
+      id: null
+    },
+    {
+      name: 'Nine of Diamonds',
+      suit: 'diamond',
+      value: 9,
+      img: 'images/diamonds/diamonds-r09.svg',
+      id: null
+    },
+    {
+      name: 'Eight of Diamonds',
+      suit: 'diamond',
+      value: 8,
+      img: 'images/diamonds/diamonds-r08.svg',
+      id: null
+    },
+    {
+      name: 'Seven of Diamonds',
+      suit: 'diamond',
+      value: 7,
+      img: 'images/diamonds/diamonds-r07.svg',
+      id: null
+    },
+    {
+      name: 'Six of Diamonds',
+      suit: 'diamond',
+      value: 6,
+      img: 'images/diamonds/diamonds-r06.svg',
+      id: null
+    },
+    {
+      name: 'Five of Diamonds',
+      suit: 'diamond',
+      value: 5,
+      img: 'images/diamonds/diamonds-r05.svg',
+      id: null
+    },
+    {
+      name: 'Four of Diamonds',
+      suit: 'diamond',
+      value: 4,
+      img: 'images/diamonds/diamonds-r04.svg',
+      id: null
+    },
+    {
+      name: 'Three of Diamonds',
+      suit: 'diamond',
+      value: 3,
+      img: 'images/diamonds/diamonds-r03.svg',
+      id: null
+    },
+    {
+      name: 'Two of Diamonds',
+      suit: 'diamond',
+      value: 2,
+      img: 'images/diamonds/diamonds-r02.svg',
+      id: null
+    },
+    {
+      name: 'Ace of Diamonds',
+      suit: 'diamond',
+      value: 14,
+      img: 'images/diamonds/diamonds-A.svg',
+      id: null
+    },
+    {
+      name: 'King of Spades',
+      suit: 'spade',
+      value: 13,
+      img: 'images/spades/spades-K.svg',
+      id: null
+  
+    },
+    {
+      name: 'Queen of Spades',
+      suit: 'spade',
+      value: 12,
+      img: 'images/spades/spades-Q.svg',
+      id: null
+    },
+    {
+      name: 'Jack of Spades',
+      suit: 'spade',
+      value: 11,
+      img: 'images/spades/spades-J.svg',
+      id: null
+    },
+    {
+      name: 'Ten of Spades',
+      suit: 'spade',
+      value: 10,
+      img: 'images/spades/spades-r10.svg',
+      id: null
+    },
+    {
+      name: 'Nine of Spades',
+      suit: 'spade',
+      value: 9,
+      img: 'images/spades/spades-r09.svg',
+      id: null
+    },
+    {
+      name: 'Eight of Spades',
+      suit: 'spade',
+      value: 8,
+      img: 'images/spades/spades-r08.svg',
+      id: null
+    },
+    {
+      name: 'Seven of Spades',
+      suit: 'spade',
+      value: 7,
+      img: 'images/spades/spades-r07.svg',
+      id: null
+    },
+    {
+      name: 'Six of Spades',
+      suit: 'spade',
+      value: 6,
+      img: 'images/spades/spades-r06.svg',
+      id: null
+    },
+    {
+      name: 'Five of Spades',
+      suit: 'spade',
+      value: 5,
+      img: 'images/spades/spades-r05.svg',
+      id: null
+    },
+    {
+      name: 'Four of Spades',
+      suit: 'spade',
+      value: 4,
+      img: 'images/spades/spades-r04.svg',
+      id: null
+    },
+    {
+      name: 'Three of Spades',
+      suit: 'spade',
+      value: 3,
+      img: 'images/spades/spades-r03.svg',
+      id: null
+    },
+    {
+      name: 'Two of Spades',
+      suit: 'spade',
+      value: 2,
+      img: 'images/spades/spades-r02.svg',
+      id: null
+    },
+    {
+      name: 'Ace of Spades',
+      suit: 'spade',
+      value: 14,
+      img: 'images/spades/spades-A.svg',
+      id: null
+    }
+  ]
+
+
+//Player's visual collected cards
+const cCollection = document.getElementById('cCollection');
+const pCollection = document.getElementById('pCollection');
+//Player's collected cards arrays
+const cCollectionArr = [];
+const pCollectionArr = [];
+
+
+//Player's visual Decks
+let cDeck = document.getElementById('cDeck');
+let pDeck = document.getElementById('pDeck');
+//Player's deck arrays
+const cDeckArr = [];
+const pDeckArr = [];
+
+
+//space where card are drawn and placed
+let cSpace = document.getElementById('cSpace');
+let pSpace = document.getElementById('pSpace');
+
+let cWarZone = document.getElementById('c-war-zone');
+let pWarZone = document.getElementById('p-war-zone');
+
+//
+let cCard = null;
+let pCard = null;
+
+
+let cScoreCalc = 0;
+let pScoreCalc = 0;
+
+
+let cScore = document.getElementById('cScore');
+let pScore = document.getElementById('pScore');
+
+let winner = null;
+
+
+
+init();
+function init() {
+    //shuffle cards
+    cardsArr.sort(() => 0.5 - Math.random());
+    //deal cards
+    cardsArr.forEach(function(card, idx){
+        if(idx % 2 === 0) {
+            cDeckArr.push(card);
+        } else {
+            pDeckArr.push(card);
+        // }
+    });
+
+    setScore();
+    render();
+}
+
+//click to deal top of each player's deck
+pDeck.addEventListener('click', dealTopCard);
+
+function dealTopCard() {
+    cCard = cDeckArr.pop();
+    pCard = pDeckArr.pop();
+    cSpace.style.backgroundImage = `url(${cCard.img})`;
+    pSpace.style.backgroundImage = `url(${pCard.img})`;
+    compareCards();
+}
+
+
+
+function compareCards() {
+    console.log(cCard.value);
+    console.log(pCard.value);
+
+    if(cCard.value > pCard.value) {
+        //push cards into cCollectionArr
+        cCollectionArr.push(cCard, pCard);
+
+
+        console.log('cDeckArr', cDeckArr);
+        console.log('cCollectionArr', cCollectionArr);
+        setTimeout(awardCards, 1000);
+
+        function awardCards() {
+            cCollection.style.backgroundImage = `url(${cCard.img})`;
+            cCollection.style.backgroundImage = `url(${pCard.img})`;
+        }
+
+
+        setTimeout(clearSpaces, 1000);
+        
+    } else if (cCard.value < pCard.value) {
+        //push cards into pCollectionArr
+        pCollectionArr.push(cCard, pCard);
+
+        console.log('pDeckArr', pDeckArr);
+        console.log('pCollectionArr', pCollectionArr);
+
+        setTimeout(awardCards, 1000);
+
+        function awardCards() {
+            pCollection.style.backgroundImage = `url(${pCard.img})`;
+            pCollection.style.backgroundImage = `url(${cCard.img})`;
+        }
+
+        setTimeout(clearSpaces, 1000);
+        
+    } else if (cCard.value === pCard.value) {
+        war();
+    }
+}
+
+
+//When deck end, push collections back into deck
+if (cDeckArr === 0) {
+    cDeckArr.push(...cCollectionArr);
+    cCollection.backgroundImage = "";
+}
+
+if(pDeckArr === 0) {
+    pDeckArr.push(...pCollectionArr);
+    pCollection.backgroundImage = "";
+}
+
+
+
+function clearSpaces() {
+    cSpace.style.backgroundImage = '';
+    pSpace.style.backgroundImage = '';
+    setScore();
+}
+
+function war() {
+    // console.log('war!');
+    // let i = 0;
+    // while(i < 4) {
+    //     // let margin = i += 2;
+    //     cWarZone.style.visibility = 'visible';
+    //     setTimeout(function() {
+    //         cCard = cDeckArr.pop();
+    //         let cWarSpot = document.createElement('div');
+    //         cWarZone.appendChild(cWarSpot);
+    //         // cWarSpot.style.marginRight = margin;
+    //     }, 1000);
+    //     pWarZone.style.visibility = 'visible';
+    //     setTimeout(function() {
+    //         pCard = pDeckArr.pop();
+    //         let pWarSpot = document.createElement('div');
+    //         pWarZone.appendChild(pWarSpot);
+    //         // pWarSpot.style.marginLeft = margin;
+    //     }, 1000);
+
+    // }  
+
+    
+}
+
+function setScore() {
+    cScoreCalc = cDeckArr.length + cCollectionArr.length;
+    pScoreCalc = pDeckArr.length + pCollectionArr.length;
+    cScore.innerHTML = `Computer has ${cScoreCalc} cards`
+    pScore.innerHTML = `Player has ${pScoreCalc} cards`
+}
+
+//if player1's card is higher than player2's card
+//player 1 gets the cards
+//else player two get them
+
+//if they deal the same card --> war
+//deal 4 four cards
+//(repeat) --> use modular code
+//if player1's card is higher than player2's card
+//player 1 gets the cards
+//else player two get them
+
+//when a player gets all the cards, they win
+//pop up modal window celebrating win
+
+
+//TODO: grab image of stack of cards
+function render() {
+    
+}
